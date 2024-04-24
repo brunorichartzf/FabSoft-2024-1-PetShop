8380fa6026e36c4854d0bd3d1ca6f35267e641ea
Gabriel26-univille <132339190+Gabriel26-univille@users.noreply.github.com>
2024-04-23 21:52:06 -0300

    Insert cliente base

--- src/main/java/br/univille/fabsoft_2024_1_petshop/controller/ClienteController.java
+++ src/main/java/br/univille/fabsoft_2024_1_petshop/controller/ClienteController.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.univille.fabsoft_2024_1_petshop.entity.Cliente;
 import br.univille.fabsoft_2024_1_petshop.service.ClienteService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -24,5 +28,17 @@ public class ClienteController {
     public ModelAndView index(){
         var listaClientes = service.getAll();
         return new ModelAndView("cliente/index","listaClientes",listaClientes);
+    }
+
+    @GetMapping("/novo")
+    public ModelAndView novo(){
+        var cliente = new Cliente();
+        return new ModelAndView("cliente/form","cliente",cliente);
+    }
+
+    @PostMapping
+    public ModelAndView save(Cliente cliente){
+        service.save(cliente);
+        return new ModelAndView("redirect:/clientes");
     }
 }
