62b22c7d7c9a87a41d01fc10ec8c877b740b489b
Gabriel26-univille <132339190+Gabriel26-univille@users.noreply.github.com>
2024-07-02 16:14:26 -0300

    Update forms

--- src/main/java/br/univille/fabsoft_2024_1_petshop/Startup.java
+++ src/main/java/br/univille/fabsoft_2024_1_petshop/Startup.java
@@ -1,5 +1,7 @@
 package br.univille.fabsoft_2024_1_petshop;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,25 +27,6 @@ public class Startup {
     
     @EventListener
     public void onApplicationEvent(ContextRefreshedEvent event){
-
-        var cliente1 = new Cliente();
-        cliente1.setNome("Cliente1");
-        cliente1.setTelefone(1234567890);
-        cliente1.setEmail("cliente@email.com");
-        cliente1.setEndereco("Rua 123");
-        cliente1.setDataNascimento(new Date(2024,04,17));
-        service.save(cliente1);
-
-        var pet1 = new Pet();
-        pet1.setNome("Pet1");
-        pet1.setEspecie("Cachorro");
-        pet1.setRaca("só deus sabe");
-        pet1.setCliente(cliente1);
-        petService.save(pet1);
-
-        var funcionario1 = new Funcionario();
-        funcionario1.setNome("Funcionário1");
-        funcionarioService.save(funcionario1);
 
     }
 }
